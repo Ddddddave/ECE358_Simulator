8daa4639620420aedce48cb956f8b5c152e6e64e
Alexander Mok <a8mok@uwaterloo.ca>
2015-10-07 19:09:43 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import java.util.*;
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
+	Queue buffer = new LinkedList<Double>();
+	static double queueType;
+	static double ticks;
+	static double lambda;
+	static double packetLength;
+	static double serviceTime;
+	static double transmissionTime;
+	static int bufferSize = -1;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		queueType = Double.parseDouble(args[0]);
+		ticks = Double.parseDouble(args[1]);
+		lambda = Double.parseDouble(args[2]);
+		packetLength = Double.parseDouble(args[3]);
+		serviceTime = Double.parseDouble(args[4]);
+		transmissionTime = packetLength/serviceTime;
+			
+		//queueType == 0 means M/D/1
+		//queueType == 1 means M/D/1/K 
+		if(queueType == 1){
+			bufferSize = Integer.parseInt(System.console().readLine("Enter buffer size: "));
+		}
+		
 		for(int i = 0; i < 20; i++){
 			System.out.println(exponential_rv(5));
 		}
