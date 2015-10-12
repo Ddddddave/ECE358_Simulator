d1ac7e3a70cab6cad73dfd938256201355d061b8
dave <j298liu@uwaterloo.ca>
2015-10-12 01:03:30 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,16 @@ public class Main {
 	static double packetLength;
 	static double serviceTime;
 	static double transmissionTime;
+	static double currentlyServing;
+	static double runningQueueSizeSum;
+	static double queueSizeCtr;
+	static double runningDelaySizeSum;
+	static double delaySizeCtr;
+	static double runningIdelSizeSum;
+	
 	static int bufferSize = -1;
+	static int t_arrival;
+	static int t_departure;
 	
 	public static void main(String[] args) {
 		queueType = Double.parseDouble(args[0]);
@@ -42,6 +51,15 @@ public class Main {
 	public static double exponential_rv(double lambda){
 		//(-1/lambda)* ln(1-U)
 		return (-1/lambda)* Math.log(1- Math.random());
+	}
+	
+	public void arrical(double t){
+		t_arrival--;
+		if(t_arrival <= 0){
+			if((bufferSize == -1)||(bufferSize>buffer.size())){
+				if(currentlyServing == -1)
+			}
+		}
 	}
 
 }
